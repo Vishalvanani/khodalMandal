5915be36f0d4c65c5a1217c96615ca105ae6845d
Vishal <vishalvanani82@gmail.com>
2024-09-03 22:02:52 +0530

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.khodalmandal',
+  appName: 'khodalmandal',
+  webDir: 'www'
+};
+
+export default config;
